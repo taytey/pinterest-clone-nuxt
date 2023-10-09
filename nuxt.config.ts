5db6d607c69fb606b28167a377904fec448dc1d2
Tayte Bitton <82904604+taytey@users.noreply.github.com>
2023-10-09 14:30:57 -0400

    working database call

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,8 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
+    '@nuxt/image'
+    
   ],
 
 })
--- pages/board.vue
+++ pages/board.vue
@@ -1,29 +1,27 @@
 <template>
     <div>
-      <div v-for="image in images">
-        <img :src="image.imageSrc" alt="Image from database" />
+      <div v-for="image in images" :key="image.id">
+        <img :src="image.imageSrc" alt="image from database" />
       </div>
     </div>
   </template>
   
-  <script setup>
+<script setup>
   import { ref, onMounted } from 'vue';
   
   const images = ref([]);
   
-  onMounted(async () => {
-    try {
-      const response = await fetch('/api/images');
-      if (response.ok) {
-        images.value = await response.json();
-        console.log(images.value)
-      } else {
-        console.error("Error fetching images:", await response.text());
-      }
-    } catch (error) {
-      console.error("Error fetching images:", error);
-    }
-  });
-  </script>
+  import { createClient } from '@supabase/supabase-js'
+const supabase = createClient("https://tceaemmtpjrkvqnscbli.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWFlbW10cGpya3ZxbnNjYmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MzAxOTYsImV4cCI6MjAxMTUwNjE5Nn0.eg6lB_56RBdQM-ZII3ynazYbc-GwzwglA6JGMU0TkY4")
+
+async function getImages() {
+  const { data } = await supabase.from('images').select('imageSrc')
+  images.value = data
+}
+
+onMounted(() => {
+  getImages()
+})
+</script>
   
   
