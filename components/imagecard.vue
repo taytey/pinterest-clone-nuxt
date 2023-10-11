3803122cb33466525067429c5f850eba31c85e61
Tayte Bitton <82904604+taytey@users.noreply.github.com>
2023-10-11 15:16:42 -0400

    save

---
+++ components/imagecard.vue
@@ -0,0 +1,8 @@
+<script setup>
+    const isOpen = ref(false);
+</script>
+<template>
+    <UModal v-model="isOpen">
+        <h1>I am a modal</h1>
+    </UModal>
+</template>
--- pages/board.vue
+++ pages/board.vue
@@ -1,27 +1,33 @@
 <template>
-    <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-      <div v-for="image in images" :key="image.id">
-        <img class="h-auto max-w-full rounded-lg" :src="image.imageSrc" alt="image from database" />
+  <div>
+    <!--Todo: Pass data from the link to the modal to display the correct information from the database for the image clicked-->
         
-      </div>
     </div>
-  </template>
+    <div class="py-20 pl-10 pr-10 columns-2 md:columns-5 lg-columns-4">
+      <div v-for="image in images" :key="image.id" class="mb-4">
+            <NuxtImg class="hover:opacity-50 transition-all h-auto max-w-full rounded-lg" :src="image.imageSrc" alt="image from database" />
+        </div>
+      </div>
+</template>
   
 <script setup>
   import { ref, onMounted } from 'vue';
   
   const images = ref([]);
-  
+    
   import { createClient } from '@supabase/supabase-js'
-const supabase = createClient("https://tceaemmtpjrkvqnscbli.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWFlbW10cGpya3ZxbnNjYmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MzAxOTYsImV4cCI6MjAxMTUwNjE5Nn0.eg6lB_56RBdQM-ZII3ynazYbc-GwzwglA6JGMU0TkY4")
+  const supabase = createClient("https://tceaemmtpjrkvqnscbli.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZWFlbW10cGpya3ZxbnNjYmxpIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTU5MzAxOTYsImV4cCI6MjAxMTUwNjE5Nn0.eg6lB_56RBdQM-ZII3ynazYbc-GwzwglA6JGMU0TkY4")
 
 async function getImages() {
-  const { data } = await supabase.from('images').select('imageSrc')
-  images.value = data
+    //imagesLoaded to be used to transition images in when they are fully loaded
+    const { data } = await supabase.from('images').select('imageSrc')
+    images.value = data
 }
+
 
 onMounted(() => {
   getImages()
+
 })
 </script>
   
